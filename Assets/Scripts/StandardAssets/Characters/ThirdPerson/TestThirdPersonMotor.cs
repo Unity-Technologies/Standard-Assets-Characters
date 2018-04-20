e39d7d7bff99d0f6e3d7a9f5f18d07c23974fbf1
Alec <alec@24bitgames.com>
2018-04-20 12:03:22 +0200

    Created dummy test third person motor

---
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/TestThirdPersonMotor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.ThirdPerson
+{
+	public class TestThirdPersonMotor : MonoBehaviour, IThirdPersonMotor
+	{
+		public float forwardMovement = 1f;
+		
+		public float turningSpeed { get; private set; }
+		public float lateralSpeed { get; private set; }
+
+		public float forwardSpeed
+		{
+			get { return forwardMovement; }
+		}
+	}
+}
