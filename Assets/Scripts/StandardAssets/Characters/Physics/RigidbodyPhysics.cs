4956182692cdb0a793a6e47daf27771e7c977d1a
Alec <alec@24bitgames.com>
2018-04-20 11:03:14 +0200

    Added basic Rigidbody physic character

---
+++ Assets/Scripts/StandardAssets/Characters/Physics/RigidbodyPhysics.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.Physics
+{
+	[RequireComponent(typeof(Rigidbody))]
+	public class RigidbodyPhysics : MonoBehaviour, IPhysics
+	{
+		Rigidbody m_Rigidbody;
+
+		void Awake()
+		{
+			m_Rigidbody = GetComponent<Rigidbody>();
+		}
+
+		public void Move(Vector3 moveVector3)
+		{
+			m_Rigidbody.MovePosition(transform.position + moveVector3);
+		}
+	}
+}
