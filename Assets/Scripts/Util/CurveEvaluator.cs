02e8034fafaf848fab702298cecb9babef710f60
Alec <alec@24bitgames.com>
2018-04-26 09:55:28 +0200

    Added some curve utils

---
+++ Assets/Scripts/Util/CurveEvaluator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Util
+{
+	[Serializable]
+	public class CurveEvaluator
+	{
+		public AnimationCurve curve = AnimationCurve.Linear(0,0,1,1);
+		public float maxValue = 1f;
+		
+		public virtual float Evaluate(float currentValue)
+		{
+			return curve.Evaluate(currentValue / maxValue);
+		}
+	}
+}
---
+++ Assets/Scripts/Util/MinMaxCurveEvaluator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Util
+{
+	[Serializable]
+	public class MinMaxCurveEvaluator : CurveEvaluator
+	{
+		public float minValue = 0f;
+
+		public override float Evaluate(float currentValue)
+		{
+			float fullScale = maxValue - minValue;
+			float currentScale = currentValue - minValue;
+			return curve.Evaluate(currentScale / fullScale);
+		}
+	}
+}
