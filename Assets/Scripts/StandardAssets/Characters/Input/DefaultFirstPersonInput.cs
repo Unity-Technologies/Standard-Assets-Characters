8c86841a9b8b9a2b42a9f25ae6a88070ed3032a0
Alec <alec@24bitgames.com>
2018-05-02 08:19:08 +0200

    Added base accelerate/decelerate implementations to the FPS motor

--- Assets/Scripts/StandardAssets/Characters/Input/DefaultFirstPersonInput.cs
+++ Assets/Scripts/StandardAssets/Characters/Input/DefaultFirstPersonInput.cs
@@ -1,10 +1,24 @@
 ﻿using UnityEngine;
+using UnityInput = UnityEngine.Input;
 
 namespace StandardAssets.Characters.Input
 {
 	public class DefaultFirstPersonInput : MonoBehaviour, IFirstPersonInput
 	{
-		public Vector2 moveInput { get; private set; }
-		public bool isMoveInput { get; private set; }
+		Vector2 m_MoveInput;
+
+		public Vector2 moveInput
+		{
+			get { return m_MoveInput; }
+		}
+		public bool isMoveInput 
+		{ 
+			get { return moveInput.sqrMagnitude > 0; }
+		}
+
+		void Update()
+		{
+			m_MoveInput.Set(UnityInput.GetAxis("Horizontal"), UnityInput.GetAxis("Vertical"));
+		}
 	}
 }
