1b8f8e691a586028314591d0d5c9ee9f9b1e49ab
Alec <alec@24bitgames.com>
2018-05-03 12:40:02 +0200

    Added StrafeTest

--- Assets/Scripts/StandardAssets/Characters/ThirdPerson/TestThirdPersonMotor.cs
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/TestThirdPersonMotor.cs
@@ -5,9 +5,14 @@ namespace StandardAssets.Characters.ThirdPerson
 	public class TestThirdPersonMotor : MonoBehaviour, IThirdPersonMotor
 	{
 		public float forwardMovement = 1f;
+		public float lateralMovement = 0f;
 		
 		public float turningSpeed { get; private set; }
-		public float lateralSpeed { get; private set; }
+		
+		public float lateralSpeed 
+		{
+			get { return lateralMovement; }
+		}
 
 		public float forwardSpeed
 		{
