553256fe3c98f64e48545dc06b9e9eb2768db604
Stephnie <stephnie.vn@gmail.com>
2018-05-14 14:48:14 +0200

    Target shooting scripts using timer for bullet projectile.

---
+++ Assets/ClayShootingController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClayShootingController : MonoBehaviour {
+	
+		/// <summary>
+		/// Gets required components
+		/// </summary>
+ public Rigidbody clayBullet;
+ //public int direction;
+ public Transform spawnLocation;
+ public int speed; 
+ private float counter = 5;
+	void Update () {
+	
+	 counter -= Time.deltaTime; 
+
+		if(counter < 0){
+			Shoot ();
+			counter = 5;
+		}
+		
+	}
+
+	void Shoot(){
+
+		Rigidbody shootBullet = Instantiate(clayBullet, spawnLocation.position, spawnLocation.rotation);
+		shootBullet.velocity =  (spawnLocation.forward * speed);
+		Debug.Log ("is shooting them bullets");
+	}
+
+
+}
