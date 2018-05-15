0540e758aab4593ea58e30562598a27d513042ac
Dave <david@24bitgames.com>
2018-05-15 14:34:06 +0200

    Added initial thridperson camera management

---
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/IThirdPersonMotorStateMachine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StandardAssets.Characters.ThirdPerson
+{
+    public interface IThirdPersonMotorStateMachine
+    {
+        Action idling { get; set; }
+        Action walking { get; set; }
+        Action running { get; set; }
+    }
+}
