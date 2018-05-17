1a1c621c23d556767703a20db956efa7193481d7
Alec <alec@24bitgames.com>
2018-05-17 11:35:53 +0200

    Added demo menu

---
+++ Assets/Scripts/Demo/DemoMenuItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Demo
+{
+	public class DemoMenuItem : MonoBehaviour
+	{
+		public string sceneName;
+
+		public void Clicked()
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+}
