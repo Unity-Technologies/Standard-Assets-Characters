9caf37304a553b42979f434c376729f7f55e96d3
Alec <alec@24bitgames.com>
2018-05-17 12:02:29 +0200

    Added go to menu to the various demo scenes

---
+++ Assets/Scripts/Demo/GoToMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Demo
+{
+	public class GoToMenu : MonoBehaviour
+	{
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
+}
