f55bfd2935ae568406b3e1579270ccbb7d0f52a1
Alec <alec@24bitgames.com>
2018-05-21 12:56:04 +0200

    Added movement zones - trigger implementation

---
+++ Assets/Scripts/StandardAssets/Characters/Effects/MovementZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.Effects
+{
+	/// <summary>
+	/// An abstract representation of MovementZone
+	/// </summary>
+	public abstract class MovementZone : MonoBehaviour
+	{
+		/// <summary>
+		/// The library of events to be played 
+		/// </summary>
+		public MovementEventLibrary library;
+
+		/// <summary>
+		/// Helper method for triggering movement events
+		/// </summary>
+		/// <param name="listener"></param>
+		protected void Trigger(MovementEventListener listener)
+		{
+			listener.SetCurrentMovementEventLibrary(library);
+		}
+	}
+}
---
+++ Assets/Scripts/StandardAssets/Characters/Effects/TriggerMovementZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.Effects
+{
+	/// <inheritdoc />
+	[RequireComponent(typeof(Collider))]
+	public class TriggerMovementZone : MovementZone
+	{
+		/// <summary>
+		/// Change the movement event library on trigger enter
+		/// </summary>
+		/// <param name="other"></param>
+		void OnTriggerEnter(Collider other)
+		{
+			MovementEventListener listener = other.GetComponent<MovementEventListener>();
+			if (listener != null)
+			{
+				Trigger(listener);
+			}
+		}
+	}
+}
