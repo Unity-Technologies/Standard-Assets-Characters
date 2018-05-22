c5c155ab889992e0a26f96be4c9b482960ee0073
Dave <david@24bitgames.com>
2018-05-22 15:00:47 +0200

    New Input Response Class

---
+++ Assets/Scripts/StandardAssets/Characters/Input/NewInputResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace StandardAssets.Characters.Input
+{
+    public class NewInputResponse : InputResponse
+    
+    
+    {
+        
+        /// <summary>
+        /// Classification of the type of response
+        /// </summary>
+        public DefaultInputResponseBehaviour behaviour;
+        
+        public override void Init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Tick()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
