c64d2ad8ced366fe462fdf1e9ccf8a4afdd22f04
Stephnie <stephnie.vn@gmail.com>
2018-05-22 13:38:01 +0200

    Particle movement event player added to FPS

---
+++ Assets/Scripts/StandardAssets/Characters/Effects/ParticleMovementEventPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.Effects
+{
+	public class ParticleMovementEventPlayer : MovementEventPlayer 
+	{
+		/// <summary>
+		/// Particles to be emitted
+		/// </summary>
+		public ParticleSystem partSource;
+		
+		/// <inheritdoc />
+		protected override void PlayMovementEvent(MovementEvent movementEvent)
+		{
+			partSource.Play();
+		}
+
+	}
+}
