319c363fbe1968d2ce4ae5eeaedbdb23128546b8
Alec <alec@24bitgames.com>
2018-05-22 17:32:21 +0200

    Added collider movement detection

---
+++ Assets/Scripts/StandardAssets/Characters/Effects/ColliderMovementDetection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+
+namespace StandardAssets.Characters.Effects
+{
+	/// <summary>
+	/// Used in conjunction with Colliders to detect movement
+	/// e.g. BoxColliders on the feet for ThirdPerson character
+	/// </summary>
+	[RequireComponent(typeof(Collider))]
+	public class ColliderMovementDetection : MonoBehaviour
+	{
+		/// <summary>
+		/// The movement event id
+		/// </summary>
+		public string id;
+		
+		/// <summary>
+		/// Fired when movement is detected
+		/// </summary>
+		public event Action<MovementEvent> detection;
+
+		/// <summary>
+		/// Whether or not the attached collider is a trigger
+		/// </summary>
+		bool m_IsTrigger;
+
+		void Awake()
+		{
+			//Is the Collider a Trigger
+			m_IsTrigger = GetComponent<Collider>().isTrigger;
+		}
+
+		/// <summary>
+		/// Handle triggering
+		/// </summary>
+		/// <param name="other"></param>
+		void OnTriggerEnter(Collider other)
+		{
+			if (!m_IsTrigger)
+			{
+				return;
+			}
+			
+			MovementEvent movementEvent = new MovementEvent();
+			movementEvent.id = id;
+			//TODO set position
+			OnDetection(movementEvent);
+		}
+
+		/// <summary>
+		/// Handle colliding
+		/// </summary>
+		/// <param name="other"></param>
+		void OnCollisionEnter(Collision other)
+		{
+			if (m_IsTrigger)
+			{
+				return;
+			}
+			
+			MovementEvent movementEvent = new MovementEvent();
+			movementEvent.id = id;
+			//TODO set position
+			OnDetection(movementEvent);
+		}
+
+		/// <summary>
+		/// Safely broadcast detection
+		/// </summary>
+		/// <param name="movementEvent"></param>
+		void OnDetection(MovementEvent movementEvent)
+		{
+			if (detection != null)
+			{
+				detection(movementEvent);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/StandardAssets/Characters/Effects/ColliderMovementEventBroadcaster.cs
@@ -0,0 +1,24 @@
+﻿namespace StandardAssets.Characters.Effects
+{
+	/// <summary>
+	/// Uses ColliderMovementDetection objects to broadcast movement events. i.e. footfalls
+	/// </summary>
+	public class ColliderMovementEventBroadcaster : MovementEventBroadcaster
+	{
+		/// <summary>
+		/// The movement detections
+		/// </summary>
+		public ColliderMovementDetection[] movementDetections;
+
+		/// <summary>
+		/// Subscribe to the movement detection events
+		/// </summary>
+		void Awake()
+		{
+			foreach (ColliderMovementDetection colliderMovementDetection in movementDetections)
+			{
+				colliderMovementDetection.detection += OnMoved;
+			}
+		}
+	}
+}
