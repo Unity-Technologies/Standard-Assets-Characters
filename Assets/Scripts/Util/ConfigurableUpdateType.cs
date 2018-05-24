e750a3e7503fca568fb11f6596ffc601d39aea82
Alec <alec@24bitgames.com>
2018-05-24 10:34:24 +0200

    Added configurable update behaviour

---
+++ Assets/Scripts/Util/ConfigurableUpdateType.cs
@@ -0,0 +1,12 @@
+﻿namespace Util
+{
+	/// <summary>
+	/// Defines when ConfigurableUpdate runs
+	/// </summary>
+	public enum ConfigurableUpdateType
+	{
+		Update,
+		LateUpdate,
+		FixedUpdate
+	}
+}
