9fadcee9f97e4db047dd242547443dcfe4dde41a
Alec <alec@24bitgames.com>
2018-06-08 15:08:12 +0200

    Refactor third person camera menu

---
+++ Assets/Scripts/Demo/CameraConfigUi.cs
@@ -0,0 +1,96 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Demo
+{
+	public abstract class CameraConfigUi<T> : MonoBehaviour where T : CinemachineVirtualCameraBase
+	{
+		/// <summary>
+		/// Camera parameters 
+		/// </summary>
+		public bool invertXAxis { get; set; }
+		public bool invertYAxis { get; set; }
+
+		/// <summary>
+		/// Cinemachine FreeLook Cameras
+		/// </summary>
+		public T[] cameras;
+
+		/// <summary>
+		/// UI Label Elements 
+		/// </summary>
+		public Text verticalSliderValueText;
+
+		public Text horizontalSliderValueText;
+
+		/// <summary>
+		/// UI Interactive elements 
+		/// </summary>
+		public Toggle xAxisToggle;
+
+		public Toggle yAxisToggle;
+
+		public Slider horizontalSlider;
+		public Slider verticalSlider;
+
+		protected float m_XAxisMaxSpeed;
+		protected float m_YAxisMaxSpeed;
+		
+
+		protected virtual void SetMaxSpeedSliderInitialValues(Slider slider, float value, Text sliderText, float scaledValue)
+		{
+			slider.value = value;
+			sliderText.text = scaledValue.ToString("0");
+		}
+
+		/// <summary>
+		/// Sets the vertical sensitivity
+		/// </summary>
+		public void SetVerticalSliderValue(Slider slider)
+		{
+			m_YAxisMaxSpeed = ((slider.value * 5) + 1);
+			verticalSliderValueText.text = m_YAxisMaxSpeed.ToString("0");
+			foreach (var camera in cameras)
+			{
+				SetYAxisMaxSpeed(camera, m_YAxisMaxSpeed);
+			}
+		}
+
+		/// <summary>
+		/// Sets the horizontal sensisitivity
+		/// </summary>
+		public void SetHorizontalSliderValue(Slider slider)
+		{
+			m_XAxisMaxSpeed = ((slider.value * 300) + 100);
+			horizontalSliderValueText.text = m_XAxisMaxSpeed.ToString("0");
+			foreach (var camera in cameras)
+			{
+				SetXAxisMaxSpeed(camera, m_XAxisMaxSpeed);
+			}
+		}
+
+		public void ToggleXAxis(Toggle toggle)
+		{
+			invertXAxis = toggle.isOn;
+			foreach (var camera in cameras)
+			{
+				InvertXAxis(camera, invertXAxis);
+			}
+		}
+
+		public void ToggleYAxis(Toggle toggle)
+		{
+			invertYAxis = toggle.isOn;
+			foreach (var camera in cameras)
+			{
+				InvertYAxis(camera, invertYAxis);
+			}
+		}
+
+		protected abstract void SetYAxisMaxSpeed(T camera, float newMaxYAxisMaxSpeed);
+		protected abstract void SetXAxisMaxSpeed(T camera, float newMaxXAxisMaxSpeed);
+		protected abstract void InvertXAxis(T camera, bool invert);
+		protected abstract void InvertYAxis(T camera, bool invert);
+	}
+}
---
+++ Assets/Scripts/Demo/FreeLookCameraConfigUi.cs
@@ -0,0 +1,43 @@
+﻿using Cinemachine;
+using UnityEngine.UI;
+
+namespace Demo
+{
+	public class FreeLookCameraConfigUi : CameraConfigUi<CinemachineFreeLook>
+	{
+		protected virtual void Start()
+		{
+			//SetInitalValues for cameraInversion toggles
+			xAxisToggle.isOn = cameras[0].m_XAxis.m_InvertInput;
+			yAxisToggle.isOn = cameras[0].m_YAxis.m_InvertInput;
+
+			m_XAxisMaxSpeed = (cameras[0].m_XAxis.m_MaxSpeed - 100) / 300;
+			SetMaxSpeedSliderInitialValues(horizontalSlider, m_XAxisMaxSpeed, horizontalSliderValueText,
+			                               ((m_XAxisMaxSpeed * 300) + 100));
+
+			m_YAxisMaxSpeed = (cameras[0].m_YAxis.m_MaxSpeed - 1) / 5;
+			SetMaxSpeedSliderInitialValues(verticalSlider, m_YAxisMaxSpeed, verticalSliderValueText,
+			                               ((m_YAxisMaxSpeed * 5) + 1));
+		}
+		
+		protected override void SetYAxisMaxSpeed(CinemachineFreeLook camera, float newMaxYAxisMaxSpeed)
+		{
+			camera.m_YAxis.m_MaxSpeed = m_YAxisMaxSpeed;
+		}
+
+		protected override void SetXAxisMaxSpeed(CinemachineFreeLook camera, float newMaxXAxisMaxSpeed)
+		{
+			camera.m_XAxis.m_MaxSpeed = m_XAxisMaxSpeed;
+		}
+
+		protected override void InvertXAxis(CinemachineFreeLook camera, bool invert)
+		{
+			camera.m_XAxis.m_InvertInput = invertXAxis;
+		}
+
+		protected override void InvertYAxis(CinemachineFreeLook camera, bool invert)
+		{
+			camera.m_YAxis.m_InvertInput = invertYAxis;
+		}
+	}
+}
