58bcd3585cac89da3ed1088ed13113fc58a64afa
Kevin Marais <kevin@24bitgames.com>
2018-06-14 08:51:10 +0200

    [PhysicsThirdPerson ] Added states to motor for a smoother rapid 180 turn Added a sprint toggle

---
+++ Assets/Scripts/StandardAssets/Characters/Physics/PhysicsMotorProperties.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.Physics
+{
+	/// <summary>
+	/// Physics motor properties for the Physics Third Person Motor. eg walk speed's proportion of max speed
+	/// </summary>
+	[CreateAssetMenu(fileName = "PhysicsMotorProperties", menuName = "Physics motor properties", order = 1)]
+	public class PhysicsMotorProperties : ScriptableObject
+	{
+		[SerializeField, Range(0, 1f)] 
+		private float	walkSpeedProporitonOfMaxSpeed = 0.5f,
+						runSpeedProportionOfMaxSpeed = 1.0f;
+		
+		[SerializeField, Range(0, 1f)] 
+		private float	walkAccelerationProporitonOfMaxSpeed = 0.5f,
+						runAccelerationProportionOfMaxSpeed = 1.0f;
+
+		public float walkSpeedProporiton
+		{
+			get { return walkSpeedProporitonOfMaxSpeed; }
+		}
+
+		public float runSpeedProportion
+		{
+			get { return runSpeedProportionOfMaxSpeed; }
+		}
+		
+		public float walkAccelerationProporiton
+		{
+			get { return walkAccelerationProporitonOfMaxSpeed; }
+		}
+
+		public float runAccelerationProportion
+		{
+			get { return runAccelerationProportionOfMaxSpeed; }
+		}
+	}
+}
