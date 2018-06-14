ef39324667f63d79e0a825e9184a6ceca7cfb215
Dave <david@24bitgames.com>
2018-06-14 11:26:47 +0200

    Added accessor for 3RDPerson motor to access and set MAX speed. Added max speed UI into legacy+newInput 3rd person Ui

---
+++ Assets/Scripts/Demo/ThirdPersonMovementSpeedConfigUi.cs
@@ -0,0 +1,48 @@
+﻿using StandardAssets.Characters.ThirdPerson;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Demo
+{
+    public class ThirdPersonMovementSpeedConfigUi: MonoBehaviour
+    {
+        public Text currentMovementSpeedText;
+        public PhysicsThirdPersonMotor thirdPersonPhysicsMotor;
+
+        
+
+        public Slider speedSlider;
+
+        private float currentSpeed;
+        
+
+        private float minSpeed = 1;
+        private float maxSpeed = 15;
+        private float speedRange = 14;
+
+       
+        private void Update()
+        {
+            UpdateSpeedSlider();
+        }
+        
+        /// <summary>
+        /// Updates UI Label with current state max speed
+        /// </summary>
+        void UpdateSpeedSlider()
+        {
+          //  Debug.Log(thirdPersonPhysicsMotor.currentSpeedForUIget);
+            
+           currentSpeed = thirdPersonPhysicsMotor.currentSpeedForUi;
+           currentMovementSpeedText.text = currentSpeed.ToString("0.0");
+           speedSlider.value = (currentSpeed - minSpeed) / speedRange;
+            
+        }
+
+        public void UpdateSpeedForCurrentState(Slider slider)
+        {
+            float speed = (slider.value*speedRange)+minSpeed;
+           thirdPersonPhysicsMotor.currentSpeedForUi = speed;
+        }
+    }
+}
