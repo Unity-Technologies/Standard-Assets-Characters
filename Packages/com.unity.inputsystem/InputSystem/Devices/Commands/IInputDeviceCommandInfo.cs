85d3ce1f29fc242bc28416e4e33556e71a60e855
Dave <david@24bitgames.com>
2018-06-25 10:58:27 +0200

    Removed com.unity.inputsystem packagae

--- Packages/com.unity.inputsystem/InputSystem/Devices/Commands/IInputDeviceCommandInfo.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.Experimental.Input.Utilities;
-
-namespace UnityEngine.Experimental.Input.LowLevel
-{
-    public interface IInputDeviceCommandInfo
-    {
-        FourCC GetTypeStatic();
-    }
-}
--- Packages/com.unity.inputsystem/InputSystem/State/IInputStateChangeMonitor.cs
+++
@@ -1,8 +0,0 @@
-namespace UnityEngine.Experimental.Input.LowLevel
-{
-    public interface IInputStateChangeMonitor
-    {
-        void NotifyControlValueChanged(InputControl control, double time, long monitorIndex);
-        void NotifyTimerExpired(InputControl control, double time, long monitorIndex, int timerIndex);
-    }
-}
