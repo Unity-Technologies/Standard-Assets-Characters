0a5c47f677efa91f9d6a4c69440acc91758ef565
Dave <david@24bitgames.com>
2018-07-02 12:43:20 +0200

    Revert thridperson sprint response

--- Assets/Scripts/StandardAssets/Characters/ThirdPerson/InputThirdPersonMotor.cs
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/InputThirdPersonMotor.cs
@@ -44,7 +44,7 @@ namespace StandardAssets.Characters.ThirdPerson
 		protected float snapSpeedTarget = 0.2f;
 
 		[SerializeField]
-		protected InputResponse[] runInput;
+		protected InputResponse runInput;
 		
 		//[SerializeField]
 		//protected InputResponse[] runInput;
@@ -95,14 +95,11 @@ namespace StandardAssets.Characters.ThirdPerson
 			characterInput = GetComponent<ICharacterInput>();
 			characterPhysics = GetComponent<ICharacterPhysics>();
 
-			foreach (var runResponse in runInput)
+			if (runInput != null)
 			{
-				if (runResponse != null)
-				{
-					runResponse.Init();
-				}
+				runInput.Init();
 			}
-
+			
 			if (strafeInput != null)
 			{
 				strafeInput.Init();
@@ -150,21 +147,12 @@ namespace StandardAssets.Characters.ThirdPerson
 			characterPhysics.landed += OnLanding;
 			characterPhysics.startedFalling += OnStartedFalling;
 			
-			foreach (var runResponse in runInput)
-			{
-				if (runResponse != null)
-				{
-					runResponse.started += OnRunStarted;
-					runResponse.ended += OnRunEnded;
-				}
-			}
-			/*
-			 * if (runInput != null)
+			 if (runInput != null)
 			{
 				runInput.started += OnRunStarted;
 				runInput.ended += OnRunEnded;
 			}
-			 */
+			 
 
 			if (strafeInput != null)
 			{
@@ -199,22 +187,12 @@ namespace StandardAssets.Characters.ThirdPerson
 				characterPhysics.landed -= OnLanding;
 				characterPhysics.startedFalling -= OnStartedFalling;
 			}
-			foreach (var runResponse in runInput)
-			{
-				if (runResponse != null)
-				{
-					runResponse.started += OnRunStarted;
-					runResponse.ended += OnRunEnded;
-				}
-			}
-			
-			/*
-			 * if (runInput != null)
+			 if (runInput != null)
 			{
 				runInput.started -= OnRunStarted;
 				runInput.ended -= OnRunEnded;
 			}
-			 */
+			 
 
 			if (strafeInput != null)
 			{
@@ -356,6 +334,8 @@ namespace StandardAssets.Characters.ThirdPerson
 				Quaternion.RotateTowards(transform.rotation, targetRotation, actualTurnSpeed * Time.fixedDeltaTime);
 
 			transform.rotation = targetRotation;
+			
+			Debug.Log(transform.position);
 		}
 	}
 }
