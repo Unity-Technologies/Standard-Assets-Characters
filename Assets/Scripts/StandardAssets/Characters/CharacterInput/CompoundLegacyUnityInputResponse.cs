3c502d804dc116957560e6040f8a6b033c83c5a3
Alec <alec@24bitgames.com>
2018-07-03 15:19:45 +0200

    Added in compound input responses

---
+++ Assets/Scripts/StandardAssets/Characters/CharacterInput/CompoundLegacyUnityInputResponse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.CharacterInput
+{
+	[CreateAssetMenu(fileName = "InputResponse", menuName = "Input Response/Create Compound Unity Input Response",
+		order = 1)]
+	public class CompoundLegacyUnityInputResponse : InputResponse
+	{
+		[SerializeField]
+		protected LegacyUnityInputResponse[] inputs;
+		
+		public override void Init()
+		{
+			foreach (LegacyUnityInputResponse legacyUnityInputResponse in inputs)
+			{
+				legacyUnityInputResponse.Init();
+				legacyUnityInputResponse.started += OnInputStarted;
+				legacyUnityInputResponse.ended += OnInputEnded;
+			}
+		}
+	}
+}
