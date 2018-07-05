9042ab6d37acf66bd3eea6932de355e24b7416c5
Alec <alec@24bitgames.com>
2018-07-05 12:12:42 +0200

    Cycle offset hacks

--- Assets/Scripts/StandardAssets/Characters/ThirdPerson/RapidTurnAnimatorState.cs
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/RapidTurnAnimatorState.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace StandardAssets.Characters.ThirdPerson
 {
-	public class RapidTurnAnimatorState : StateMachineBehaviour 
+	public class RapidTurnAnimatorState : StateMachineBehaviour
 	{
+		[FormerlySerializedAs("parameterName")]
+		public string clipOffsetParameter = "";
+
+		public string clipSpeedParameter = "";
+		
 		public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 		{
 			var animationController = animator.GetComponent<ThirdPersonAnimationController>();
 			if (animationController != null)
 			{
 				animationController.OnRapidTurnComplete();
+			}
+		}
+
+		private void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+		{
+			Debug.Log(1f - animator.GetFloat(clipOffsetParameter));
+			if (animatorStateInfo.normalizedTime >= 1 - Time.deltaTime * animator.GetFloat(clipSpeedParameter) - animator.GetFloat(clipOffsetParameter))
+			{
+				animator.SetTrigger("EXIT");
 			}
 		}
 	}
