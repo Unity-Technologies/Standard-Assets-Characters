d6ed05fb7c55b1bce5b564b232b64baff9d36053
Alec <alec.projectx.larsen@gmail.com>
2018-07-06 13:28:20 +0200

    Prototyping turning around

---
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/TurnaroundBehaviour.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+namespace StandardAssets.Characters.ThirdPerson
+{
+	public class TurnaroundBehaviour : MonoBehaviour
+	{
+		[SerializeField]
+		protected float rotation = 180f;
+		
+		[SerializeField]
+		protected float timeToTurn = 0.3f;
+
+		[SerializeField]
+		protected float forwardDampTimeScale = 0.1f;
+		
+		[SerializeField]
+		protected AnimationCurve forwardSpeed = AnimationCurve.Linear(0,0,1,1);
+		
+		[SerializeField]
+		protected AnimationCurve turningSpeed = AnimationCurve.Linear(0,0,1,1);
+
+		[SerializeField]
+		protected Animator animator;
+
+		Vector3 targetRotationEuler;
+		Quaternion targetRotation;
+		private bool isTurningAround = false;
+		private float turningTime = 0f;
+		float currentForwardSpeed;
+
+		public void TurnAround()
+		{
+			if (isTurningAround)
+			{
+				return;
+			}
+
+			isTurningAround = true;
+			turningTime = 0f;
+			currentForwardSpeed = animator.GetFloat("ForwardSpeed");
+			targetRotationEuler = transform.eulerAngles;
+			targetRotationEuler.y += rotation;
+			targetRotation = Quaternion.Euler(targetRotationEuler);
+		}
+
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				TurnAround();
+			}
+			
+			if (isTurningAround)
+			{
+				EvaluateTurn();
+				turningTime += Time.deltaTime;
+				if (turningTime >= timeToTurn)
+				{
+					turningTime = timeToTurn;
+					isTurningAround = false;
+					EvaluateTurn();
+				}
+			}
+		}
+
+		void EvaluateTurn()
+		{
+			float normalizedTime = turningTime / timeToTurn;
+
+			float turningSpeedValue = turningSpeed.Evaluate(normalizedTime);
+			float forwardSpeedValue = forwardSpeed.Evaluate(normalizedTime);
+			
+			animator.SetFloat("TurningSpeed", turningSpeedValue);
+			animator.SetFloat("ForwardSpeed", forwardSpeedValue + currentForwardSpeed);
+			
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotation/timeToTurn * Time.deltaTime);
+		}
+	}
+}
