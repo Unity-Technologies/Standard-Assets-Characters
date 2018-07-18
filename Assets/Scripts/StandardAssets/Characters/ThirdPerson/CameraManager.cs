e113804c86ee246ac7288ee29007ddb4754ef568
Dave <david@24bitgames.com>
2018-07-18 17:03:55 +0200

    Set both OnStartStrafe/Action to change priority of cameras and not turn off the entire game object.

--- Assets/Scripts/StandardAssets/Characters/ThirdPerson/CameraManager.cs
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/CameraManager.cs
@@ -10,11 +10,12 @@ namespace StandardAssets.Characters.ThirdPerson
 
 		[SerializeField]
 		protected GameObject strafeSDC, actionSDC;
-
-		public CinemachineStateDrivenCamera actionCameraState;
-		public CinemachineStateDrivenCamera strafeCameraState;
-
-		private CinemachineVirtualCamera vCam;
+		
+		
+		[SerializeField]
+		protected CinemachineStateDrivenCamera actionCameraState;
+		[SerializeField]
+		protected CinemachineStateDrivenCamera strafeCameraState;
 
 		private int livePriority = 10;
 		private int standbyPriority = 1;
@@ -29,8 +30,6 @@ namespace StandardAssets.Characters.ThirdPerson
 			
 			motor.startStrafeMode += MotorOnStartStrafeMode;
 			motor.startActionMode += MotorOnStartActionMode;
-			
-			
 		}
 
 		private void OnDisable()
@@ -49,16 +48,20 @@ namespace StandardAssets.Characters.ThirdPerson
 			foreach (var cam in actionCameraState.ChildCameras)
 			{
 				cam.GetComponent<CinemachineFreeLook>().m_XAxis.Value = 0;
-				cam.GetComponent<CinemachineFreeLook>().m_YAxis.Value = strafeCameraState.ChildCameras[0].GetComponent<CinemachineFreeLook>().m_YAxis.Value;
+				cam.GetComponent<CinemachineFreeLook>().m_YAxis.Value =
+					strafeCameraState.ChildCameras[0].GetComponent<CinemachineFreeLook>().m_YAxis.Value;
 			}
 			actionCameraState.Priority = livePriority;
-            			strafeCameraState.Priority = standbyPriority;
+            strafeCameraState.Priority = standbyPriority;
 			//strafeSDC.SetActive(false);
 			//actionSDC.SetActive(true);
 		}
 
 		void MotorOnStartStrafeMode()
 		{	
+			
+			//TODO Get Strafe camera to start with the expected YAxis value
+			
 			actionCameraState.Priority = standbyPriority;
 			strafeCameraState.Priority = livePriority;
 			//strafeSDC.SetActive(true);
