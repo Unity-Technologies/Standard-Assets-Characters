4c8d8e8f76c03cd405429c204ca587721ac2d2cf
Alec <alec@24bitgames.com>
2018-08-01 11:15:17 +0200

    Made the blendspace turnaround use better defaults

---
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/BlendspaceTurnaroundConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace StandardAssets.Characters.ThirdPerson
+{
+	[Serializable]
+	public class BlendspaceTurnaroundConfiguration
+	{
+		[SerializeField]
+		protected float timeToTurn = 0.2f;
+
+		[SerializeField]
+		protected float turnSpeed = 0f;
+
+		[SerializeField]
+		protected AnimationCurve forwardSpeed = AnimationCurve.Linear(0, 1, 1, 1);
+
+		[SerializeField]
+		protected Calculation forwardSpeedCalculation = Calculation.Multiplicative;
+
+		public float turnTime
+		{
+			get { return timeToTurn; }
+		}
+
+		public float normalizedTurnSpeed
+		{
+			get { return turnSpeed; }
+		}
+		
+		public AnimationCurve forwardSpeedOverTime
+		{
+			get { return forwardSpeed; }
+		}
+
+		public Calculation forwardSpeedCalc
+		{
+			get { return forwardSpeedCalculation; }
+		}
+	}
+}
