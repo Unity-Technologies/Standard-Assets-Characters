de2e964614c5c81059d6f7c3509fa11ba04bbae4
Heinrich Mauer <heinrich@24bitgames.com>
2018-08-01 15:55:01 +0200

    Changed Conditional Include attribute to work inside serialised classes, with serialised fields as conditional markers. Changed Conditional Include Attribute to work with nearly every datatype as a conditional field Simplified constructor for ConditionalIncludeAttribute.

--- Assets/Scripts/Editor/CondtionalIncludePropertyDrawer.cs
+++ Assets/Scripts/Editor/CondtionalIncludePropertyDrawer.cs
@@ -2,6 +2,8 @@
 using Attributes;
 using UnityEditor;
 using UnityEngine;
+using System;
+
 
 namespace Editor
 {
@@ -14,30 +16,90 @@ namespace Editor
             get{ return (ConditionalIncludeAttribute)attribute; } 
         }
 
-        private bool showField;
+        private bool toShow = true;
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return toShow ? EditorGUI.GetPropertyHeight(property) : 0;
+        }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            showField = false;
-            if(includeAttribute.conditionElement != null)
+            if (!string.IsNullOrEmpty(includeAttribute.conditionField))
             {
-                var conditionIndex = property.serializedObject.FindProperty(includeAttribute.conditionField).enumValueIndex;
-                showField = (int) includeAttribute.conditionElement == conditionIndex; 
+                var conditionProperty = FindPropertyRelative(property, includeAttribute.conditionField);
+                if (conditionProperty != null)
+                {
+                    bool isBoolMatch = conditionProperty.propertyType == SerializedPropertyType.Boolean && conditionProperty.boolValue;
+                    string compareStringValue = includeAttribute.conditionElement ==  null ? string.Empty  : includeAttribute.conditionElement.ToString().ToUpper() ;
+                    if (isBoolMatch && compareStringValue == "FALSE") isBoolMatch = false;
+
+                    string conditionPropertyStringValue = conditionProperty.AsStringValue().ToUpper();
+                    bool objectMatch = compareStringValue == conditionPropertyStringValue;
+
+                    if (!isBoolMatch && !objectMatch)
+                    {
+                        toShow = false;
+                        return;
+                    }
+                }
             }
-            else
-            {
-                showField = property.serializedObject.FindProperty(includeAttribute.conditionField).boolValue;
-            }
-            
-            if (showField)
-            {
-                EditorGUI.PropertyField(position, property, label, true);
-            }
+
+            toShow = true;
+            EditorGUI.PropertyField(position, property, label, true);
         }
-        
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+
+        private SerializedProperty FindPropertyRelative(SerializedProperty property, string propertyName)
         {
-            return showField ? EditorGUI.GetPropertyHeight(property) : 0;
+            if (property.depth == 0) return property.serializedObject.FindProperty(propertyName);
+
+            var path = property.propertyPath.Replace(".Array.data[", "[");
+            var elements = path.Split('.');
+            SerializedProperty parent = null;
+
+		
+            for (int i = 0; i < elements.Length - 1; i++)
+            {
+                var element = elements[i];
+                int index = -1;
+                if (element.Contains("["))
+                {
+                    index = Convert.ToInt32(element.Substring(element.IndexOf("[", StringComparison.Ordinal)).Replace("[", "").Replace("]", ""));
+                    element = element.Substring(0, element.IndexOf("[", StringComparison.Ordinal));
+                }
+			
+                parent = i == 0 ? 
+                    property.serializedObject.FindProperty(element) : 
+                    parent.FindPropertyRelative(element);
+
+                if (index >= 0) parent = parent.GetArrayElementAtIndex(index);
+            }
+
+            return parent.FindPropertyRelative(propertyName);
+        }
+    }
+
+    public static class SerialisedPropertyExtensions
+    {
+        public static string AsStringValue(this SerializedProperty property)
+        {
+            switch (property.propertyType)
+            {
+                case SerializedPropertyType.String:
+                    return property.stringValue;
+                case SerializedPropertyType.Character:
+                case SerializedPropertyType.Integer:
+                    if (property.type == "char") return Convert.ToChar(property.intValue).ToString();
+                    return property.intValue.ToString();
+                case SerializedPropertyType.ObjectReference:
+                    return property.objectReferenceValue != null ? property.objectReferenceValue.ToString() : "null";
+                case SerializedPropertyType.Boolean:
+                    return property.boolValue.ToString();
+                case SerializedPropertyType.Enum:
+                    return property.enumNames[property.enumValueIndex];
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
