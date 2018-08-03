8e77dbc051f54a3a94b958d5b4885f1940f85cfe
Kevin Marais <kevin.n.marais@gmail.com>
2018-08-03 12:17:44 +0200

    Added startedSlide property to ICharacterPhysics

--- Assets/Scripts/StandardAssets/Characters/ThirdPerson/RootMotionThirdPersonMotor.cs
+++ Assets/Scripts/StandardAssets/Characters/ThirdPerson/RootMotionThirdPersonMotor.cs
@@ -275,7 +275,7 @@ namespace StandardAssets.Characters.ThirdPerson
 		/// </summary>
 		protected virtual void OnJumpPressed()
 		{
-			if (!characterPhysics.isGrounded || !animationController.CanJump)
+			if (!characterPhysics.isGrounded || characterPhysics.startedSlide || !animationController.CanJump)
 			{
 				return;
 			}
