8871a0886b9dfe148149cb255bac50f43ce3d37d
Dave <david@24bitgames.com>
2018-08-10 13:49:52 +0200

    removed Free look and hybrid cameras

--- Assets/Scripts/StandardAssets/Characters/Common/ThirdPersonCameraAnimationManager.cs
+++ Assets/Scripts/StandardAssets/Characters/Common/ThirdPersonCameraAnimationManager.cs
@@ -8,12 +8,7 @@ namespace StandardAssets.Characters.Common
 {
 	public class ThirdPersonCameraAnimationManager : CameraAnimationManager
 	{
-		private string[] actionCameraMode = {"World Camera", "FreeLook Camera", "Hybrid Camera"};
-		private int currentCameraModeIndex = 0;
 		private string strafeStateName = "Strafe";
-
-		[SerializeField]
-		protected InputResponse changeCameraModeInputResponse;
 
 		[SerializeField]
 		protected InputResponse recenterCameraInputResponse;
@@ -25,12 +20,6 @@ namespace StandardAssets.Characters.Common
 		protected Text currentCameraText;
 
 		[SerializeField]
-		protected CinemachineFreeLook hybridIdleCamera;
-
-		[SerializeField]
-		protected CameraAnimationManager cameraAnimationManager;
-
-		[SerializeField]
 		protected CinemachineFreeLook defaultWorldFreelookCam;
 
 		private bool recetnerHybridIdle;
@@ -38,20 +27,17 @@ namespace StandardAssets.Characters.Common
 
 		private void Awake()
 		{
-			//currentCameraText.text = actionCameraMode[currentActionModeIndex];
-			changeCameraModeInputResponse.Init();
+			
 			recenterCameraInputResponse.Init();
 		}
 
 		private void OnEnable()
 		{
-			changeCameraModeInputResponse.started += SwitchActionMode;
 			recenterCameraInputResponse.started += RecenterCamera;
 		}
 
 		private void OnDisable()
 		{
-			changeCameraModeInputResponse.started -= SwitchActionMode;
 			recenterCameraInputResponse.started -= RecenterCamera;
 		}
 
@@ -64,50 +50,18 @@ namespace StandardAssets.Characters.Common
 				RecenterFreeLookCam(defaultWorldFreelookCam);
 			}
 			
-			//For hybrid camera, if the player is moving then the camera will change
-			//To Follow mode. 
-			if (characterInput.hasMovementInput)
-			{
-				cameraAnimationManager.SetAnimation("Follow",1);
-			}
-			else
-			{
-				//if the character is not moving, and the state is in world, and the player presses recenter
-				//The camera should recenter and return to world state. 
-				if (cameraAnimationManager.ActiveState("Follow", 1))
-				{
-					cameraAnimationManager.SetAnimation("World",1);
-				}
-				RecenterFreeLookCam(hybridIdleCamera);
-				recetnerHybridIdle = true;
-			}
 		}
 
 		private void Update()
 		{
-			if (recetnerHybridIdle && characterInput.hasMovementInput)
-			{
-				cameraAnimationManager.SetAnimation("Follow",1);
-			}
-			
 			//Idle cameras will turn off recenter if there is any movement on left or 
 			//Right sticks. 
 			if (characterInput.hasMovementInput
 			    | characterInput.lookInput != Vector2.zero)
 			{
-				TurnOffFreeLookCamRecenter(hybridIdleCamera);
-				recetnerHybridIdle = false;
-				
 				TurnOffFreeLookCamRecenter(defaultWorldFreelookCam);
-			}
-
-			//The run cam will turn off recenter only if there is movmement on the right stick (look)
-			// In the case of hybrid camera (running) 
-			if (characterInput.lookInput.x > 0.1 || characterInput.lookInput.x < -0.1)
-			{
-				cameraAnimationManager.SetAnimation("World", 1);
-			}
-		}
+			}			
+	}
 
 		public void StrafeStarted()
 		{
@@ -117,8 +71,8 @@ namespace StandardAssets.Characters.Common
 
 		public void StrafeEnded()
 		{
-			SetAnimation(actionCameraMode[currentCameraModeIndex]);
-			currentCameraText.text = actionCameraMode[currentCameraModeIndex];
+			SetAnimation("World Camera");
+			currentCameraText.text = "World Camera";
 		}
 
 		/// <summary>
@@ -138,23 +92,5 @@ namespace StandardAssets.Characters.Common
 			freeLook.m_YAxisRecentering.m_enabled = false;
 		}
 
-		void SwitchActionMode()
-		{
-			//Index 0 - WorldCamera Mode
-			//Index 1 - FreelookCamera Mode
-			//Index 2 - HybridCamera Mode
-			if (currentCameraModeIndex == 0 || currentCameraModeIndex == 1)
-			{
-				currentCameraModeIndex++;
-			}
-			else if (currentCameraModeIndex == 2)
-			{
-				currentCameraModeIndex = 0;
-			}
-
-			currentCameraText.text = actionCameraMode[currentCameraModeIndex];
-			
-			SetAnimation(actionCameraMode[currentCameraModeIndex]);
-		}
 	}
 }
