3e3040e71af14743c2dfee82316d30436e771d4e
Kevin Marais <kevin.n.marais@gmail.com>
2018-08-14 16:02:57 +0200

    Corrected should apply root motion check

--- Assets/_Standard Assets/Characters/Scripts/ThirdPerson/RootMotionThirdPersonMotor.cs
+++ Assets/_Standard Assets/Characters/Scripts/ThirdPerson/RootMotionThirdPersonMotor.cs
@@ -155,7 +155,8 @@ namespace StandardAssets.Characters.ThirdPerson
 
 		private bool ShouldApplyRootMotion()
 		{
-			return characterPhysics.isGrounded && animationController.shouldUseRootMotion;
+			return characterPhysics.isGrounded && animationController.shouldUseRootMotion &&
+					!animationController.isRootMovement;
 		}
 
 		public void Init(ThirdPersonBrain brain)
