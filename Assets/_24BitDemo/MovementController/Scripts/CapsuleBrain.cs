f98ae9eacfb644d0934a674fb39e228d2d93cd16
Luke Lamothe <luke.lamothe@gmail.com>
2018-08-16 17:07:16 +0200

    Added new Prototyping Scene and did MAJOR REFACTOR of the folder structures

---
+++ Assets/_24BitDemo/MovementController/Scripts/CapsuleBrain.cs
@@ -0,0 +1,227 @@
+﻿using StandardAssets.Characters.CharacterInput;
+using StandardAssets.Characters.Common;
+using StandardAssets.Characters.Effects;
+using StandardAssets.Characters.Physics;
+using UnityEngine;
+
+namespace Demo.MovementController
+{
+    [RequireComponent(typeof(ICharacterPhysics))]
+    [RequireComponent(typeof(ICharacterInput))]
+    public class CapsuleBrain : CharacterBrain
+    {
+        /// <summary>
+        /// The state that first person motor starts in
+        /// </summary>
+        [SerializeField]
+        protected CapsuleMovementProperties startingMovementProperties;
+
+	    [SerializeField] 
+	    protected float turnSpeed = 300f;
+            
+        /// <summary>
+        /// The current motor state - controls how the character moves in different states
+        /// </summary>
+        public CapsuleMovementProperties currentMovementProperties { get; protected set; }
+       
+	    
+	    /// <summary>
+	    /// The current movement properties
+	    /// </summary>
+	    private float currentSpeed;
+
+	    /// <summary>
+	    /// The current movement properties
+	    /// </summary>
+	    private float movementTime;
+
+	    /// <summary>
+	    /// A check to see if input was previous being applied
+	    /// </summary>
+	    private bool previouslyHasInput;
+
+	    private MovementEventHandler _movementEventHandler;
+
+	    public override MovementEventHandler movementEventHandler
+	    {
+		    get { return _movementEventHandler; }
+	    }
+
+	    public override float targetYRotation { get; set; }
+
+	    protected override void Awake()
+        {
+            base.Awake();
+	        ChangeState(startingMovementProperties);
+        }
+
+        private void OnEnable()
+        {
+            characterInput.jumpPressed += OnJumpPressed;
+        }
+        
+        /// <summary>
+        /// Unsubscribe
+        /// </summary>
+        private void OnDisable()
+        {
+            if (characterInput == null)
+            {
+                return;
+            }
+			
+            characterInput.jumpPressed -= OnJumpPressed;
+        }
+        
+        /// <summary>
+		/// Handles camera rotation
+		/// </summary>
+		private void Update()
+		{
+			Quaternion targetRotation = CalculateTargetRotation();
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+
+			targetYRotation = targetRotation.eulerAngles.y;
+		}
+	    
+	    
+	    protected virtual Quaternion CalculateTargetRotation()
+	    {
+		    Vector3 flatForward = Camera.main.transform.forward;
+		    flatForward.y = 0f;
+		    flatForward.Normalize();
+
+		    Vector3 localMovementDirection =
+			    new Vector3(characterInput.moveInput.x, 0f, characterInput.moveInput.y);
+		    Quaternion cameraToInputOffset = Quaternion.FromToRotation(Vector3.forward, localMovementDirection);
+		    cameraToInputOffset.eulerAngles = new Vector3(0f, cameraToInputOffset.eulerAngles.y, 0f);
+
+		    return Quaternion.LookRotation(cameraToInputOffset * flatForward);
+	    }
+
+		/// <summary>
+		/// Handles jumping
+		/// </summary>
+		private void OnJumpPressed()
+		{
+			if (characterPhysics.isGrounded && currentMovementProperties.canJump)
+			{
+				characterPhysics.SetJumpVelocity(currentMovementProperties.jumpingSpeed);
+			}	
+		}
+
+		/// <summary>
+		/// Handles movement on Physics update
+		/// </summary>
+		private void FixedUpdate()
+		{
+			Move();
+		}
+
+		/// <summary>
+		/// State based movement
+		/// </summary>
+		private void Move()
+		{
+			if (startingMovementProperties == null)
+			{
+				return;
+			}
+
+			if (characterInput.hasMovementInput)
+			{
+				if (!previouslyHasInput)
+				{
+					movementTime = 0f;
+				}
+				Accelerate();
+			}
+			else
+			{
+				if (previouslyHasInput)
+				{
+					movementTime = 0f;
+				}
+
+				Stop();
+			}
+
+			Vector2 input = characterInput.moveInput;
+			if (input.sqrMagnitude > 1)
+			{
+				input.Normalize();
+			}
+		
+			Vector3 forward = transform.forward * input.magnitude;
+			Vector3 sideways = Vector3.zero;
+			
+			characterPhysics.Move((forward + sideways) * currentSpeed * Time.deltaTime);
+
+			previouslyHasInput = characterInput.hasMovementInput;
+		}	
+
+		/// <summary>
+		/// Calculates current speed based on acceleration anim curve
+		/// </summary>
+		private void Accelerate()
+		{
+			movementTime += Time.fixedDeltaTime;
+			movementTime = Mathf.Clamp(movementTime, 0f, currentMovementProperties.accelerationCurve.maxValue);
+			currentSpeed = currentMovementProperties.accelerationCurve.Evaluate(movementTime) * currentMovementProperties.maximumSpeed;
+		}
+		
+		/// <summary>
+		/// Stops the movement
+		/// </summary>
+		private void Stop()
+		{
+			currentSpeed = 0f;
+		}
+
+		/// <summary>
+		/// Clamps the current speed
+		/// </summary>
+		private void ClampCurrentSpeed()
+		{
+			currentSpeed = Mathf.Clamp(currentSpeed, 0f, currentMovementProperties.maximumSpeed);
+		}
+		
+		/// <summary>
+		/// Changes the current motor state and play events associated with state change
+		/// </summary>
+		/// <param name="newState"></param>
+		protected virtual void ChangeState(CapsuleMovementProperties newState)
+		{
+			if (newState == null)
+			{
+				return;
+			}
+			
+			if (currentMovementProperties != null)
+			{
+				currentMovementProperties.ExitState();
+			}
+
+			currentMovementProperties = newState;
+			currentMovementProperties.EnterState();
+		}
+
+		/// <summary>
+		/// Change state to the new state and adds to previous state stack
+		/// </summary>
+		/// <param name="newState"></param>
+		public void EnterNewState(CapsuleMovementProperties newState)
+		{
+			ChangeState(newState);
+		}
+
+		/// <summary>
+		/// Resets state to previous state
+		/// </summary>
+		public void ResetState()
+		{
+			ChangeState(startingMovementProperties);
+			
+		}
+    }
+}
---
+++ Assets/_24BitDemo/Scripts/TFBGames/Editor/AnimationControllerEditor.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace StandardAssets.TFBGames.Editor
+{
+	/// <summary>
+	/// Editor window to replace animations clips in an AnimationController
+	/// </summary>
+	public class AnimationControllerEditor : EditorWindow
+	{
+		[Serializable]
+		public struct ReplacementRule
+		{
+			public string oldValue,
+			              newValue;
+		}
+
+		public ReplacementRule[] clipNameReplacementRules;
+
+		private AnimatorController baseController;
+		private bool useBaseClipsIfNotFound;
+		private string newControllerName = "NewAnimationController";
+
+		[MenuItem("24 Bit Games/Animation Controller Clip Replacer")]
+		public static void ShowWindow()
+		{
+			GetWindow(typeof(AnimationControllerEditor));
+		}
+
+		private void ReplaceAnimationClips(BlendTree blendtree)
+		{
+			if ((blendtree == null) || (blendtree.children.Length == 0))
+			{
+				return;
+			}
+
+			if (blendtree.children[0].motion is AnimationClip)
+			{
+				int length = blendtree.children.Length;
+				ChildMotion[] newChildren = new ChildMotion[length];
+				for (int i = 0; i < length; i++)
+				{
+					newChildren[i] = blendtree.children[i];
+					newChildren[i].motion = GetCorrespondingClip((AnimationClip) blendtree.children[i].motion);
+				}
+
+				blendtree.children = newChildren;
+			}
+			else
+			{
+				foreach (ChildMotion child in blendtree.children)
+				{
+					ReplaceAnimationClips(child.motion as BlendTree);
+				}
+			}
+		}
+
+		private AnimationClip GetCorrespondingClip(AnimationClip clip)
+		{
+			string path = AssetDatabase.GetAssetPath(clip);
+			string newPath = clipNameReplacementRules.Aggregate(path, (current, rule) =>
+				                                                          current.Replace(rule.oldValue, rule.newValue));
+
+			var newClip = AssetDatabase.LoadAssetAtPath<AnimationClip>(newPath);
+			if (newClip != null)
+			{
+				Debug.LogFormat("Found replacement clip, {0}, for {1}", newPath, path);
+				return newClip;
+			}
+
+			// clip not found
+			Debug.LogErrorFormat("No clip found at {0}", newPath);
+			return useBaseClipsIfNotFound ? clip : null;
+		}
+
+		private void OnGUI()
+		{
+			baseController = EditorGUILayout.ObjectField("Base controller", baseController,
+			                                             typeof(AnimatorController), false, null) as AnimatorController;
+
+			newControllerName = EditorGUILayout.TextField("New controller name", newControllerName);
+
+			// use property drawer to elegantly draw array
+			var so = new SerializedObject(this);
+			SerializedProperty rulesProperty = so.FindProperty("clipNameReplacementRules");
+			EditorGUILayout.PropertyField(rulesProperty, true);
+			so.ApplyModifiedProperties();
+
+			useBaseClipsIfNotFound = EditorGUILayout.Toggle("Use base clip if clip not found", useBaseClipsIfNotFound);
+
+			if ((baseController != null) && GUILayout.Button("Replace clips"))
+			{
+				string pathToBase = AssetDatabase.GetAssetPath(baseController);
+				string newAssetPath = pathToBase.Replace(baseController.name, newControllerName);
+
+				var newController = AssetDatabase.LoadAssetAtPath<AnimatorController>(newAssetPath);
+				if (newController != null)
+				{
+					if (!EditorUtility.DisplayDialog("Overwrite file?",
+					                                 string.Format("A file with the name: {0} was found", newControllerName),
+					                                 "Overwrite", "Cancel"))
+					{
+						return;
+					}
+				}
+
+				AssetDatabase.CopyAsset(pathToBase, newAssetPath);
+				newController = AssetDatabase.LoadAssetAtPath<AnimatorController>(newAssetPath);
+
+				foreach (AnimatorControllerLayer layer in newController.layers)
+				{
+					TraverseStatemachineToReplaceClips(layer.stateMachine);
+				}
+			}
+		}
+
+		private void TraverseStatemachineToReplaceClips(AnimatorStateMachine stateMachine)
+		{
+			foreach (ChildAnimatorState childState in stateMachine.states)
+			{
+				var clip = childState.state.motion as AnimationClip;
+				if (clip != null)
+				{
+					childState.state.motion = GetCorrespondingClip(clip);
+					continue;
+				}
+
+				// not a clip, must be a blend tree
+				ReplaceAnimationClips(childState.state.motion as BlendTree);
+			}
+
+			foreach (ChildAnimatorStateMachine childStateMachine in stateMachine.stateMachines)
+			{
+				TraverseStatemachineToReplaceClips(childStateMachine.stateMachine);
+			}
+		}
+	}
+}
