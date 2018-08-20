0ad89c983fcb82051a6a8e70f1128d5bf781a36b
Kevin Marais <kevin.n.marais@gmail.com>
2018-08-20 15:26:43 +0200

    Added VelocityGraph.cs

---
+++ Assets/_24BitDemo/Scripts/VelocityGraph.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+namespace Demo
+{
+	public class VelocityGraph : MonoBehaviour
+	{
+		[SerializeField]
+		protected int numberBars = 240;
+
+		[SerializeField]
+		protected float barWidthPercOfScreenWidth = 0.00125f;
+
+		[SerializeField]
+		protected float barScale = 10;
+	
+		private Vector3 lastPosition;
+		private Vector3 deltaPosition;
+		private float speed;
+		private readonly List<float> deltas = new List<float>();
+		private Texture2D texture;
+
+		private void Awake()
+		{
+			texture = new Texture2D(1, 1);
+		}
+
+		private void OnDestroy()
+		{
+			Destroy(texture);
+		}
+
+		private void Update()
+		{
+			Vector3 currentPosition = transform.position;
+			deltaPosition = lastPosition - currentPosition;
+			speed = deltaPosition.magnitude / Time.deltaTime;
+			deltas.Add(speed);
+			if (deltas.Count >= numberBars)
+			{
+				deltas.RemoveAt(0);
+			}
+			lastPosition = currentPosition;
+		}
+	
+		private void OnGUI ()
+		{
+			Rect firstRect = new Rect((Screen.width * 0.5f) - (numberBars * 0.5f) * barWidthPercOfScreenWidth * 
+									  Screen.width, Screen.height * 0.8f, Screen.width * barWidthPercOfScreenWidth,
+									  Screen.height * 0.2f);
+			for (int i = 0; i < numberBars; i++)
+			{
+				var rect = new Rect(firstRect);
+				rect.x += i * (barWidthPercOfScreenWidth * Screen.width);
+				rect.height = deltas.Count <= i ? 0 : -deltas[i] * barScale;
+				GUI.DrawTexture(rect, texture);
+			}
+		
+			Rect bottomMiddle = new Rect(Screen.width * 0.45f, Screen.height * 0.8f, Screen.width * 0.1f, Screen.height * 0.2f);
+			string text = string.Format("Speed: {0:f2}\nForward: {1:f2}\nVertical: {2:f2}", speed, 
+										deltaPosition.GetMagnitudeOnAxis(transform.forward  / Time.deltaTime),
+										deltaPosition.GetMagnitudeOnAxis(transform.up)  / Time.deltaTime);
+			GUI.Label(bottomMiddle, text);
+		}
+	}
+}
