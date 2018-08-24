9e89c21b355688a652809b88ee96b00cfc31c2bc
Luke Lamothe <luke.lamothe@gmail.com>
2018-08-24 16:23:15 +0200

    Bakes lightmaps for Example scenes as they are small, and moved the NavMeshInput Example scene into Examples

---
+++ Assets/_Standard Assets/Characters/Examples/SimpleNavMeshInputController/Scripts/NavigateToMouseClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigateToMouseClick : MonoBehaviour {
+
+    public Camera Cam;
+    public NavMeshAgent navMesh;
+  
+
+	void Start () 
+	{
+	    if (Cam == null)
+	    {
+		    Cam = Camera.main;   
+	    }
+        navMesh = GetComponent < NavMeshAgent> ();
+    }
+
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast ( ray, out hit)) 
+            {
+                navMesh.SetDestination (hit.point);
+            }
+        }
+	}
+}
