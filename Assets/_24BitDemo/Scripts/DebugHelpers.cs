4e1d6b2e2ca4d5ce532bd0f959ed04ddb2edc808
Alec <alec@24bitgames.com>
2018-08-24 08:15:07 +0200

    Added DebugHelpers for messing up frame rate

---
+++ Assets/_24BitDemo/Scripts/DebugHelpers.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Demo
+{
+	public class DebugHelpers : MonoBehaviour
+	{
+		[SerializeField]
+		protected int targetFrameRate = 15;
+
+		public void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus))
+			{
+				if (Time.timeScale > 0.1f)
+				{
+					Time.timeScale -= 0.1f;
+				}
+			}
+
+			if (Input.GetKeyDown(KeyCode.Plus) || Input.GetKeyDown(KeyCode.KeypadPlus))
+			{
+				if (Time.timeScale < 1f)
+				{
+					Time.timeScale += 0.1f;
+				}
+			}
+
+			if (Input.GetKeyDown(KeyCode.L))
+			{
+				if (Application.targetFrameRate < 0)
+				{
+					Application.targetFrameRate = targetFrameRate;
+				}
+				else
+				{
+					Application.targetFrameRate = -1;
+				}
+			}
+		}
+	}
+}
