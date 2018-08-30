0246e03c4a9b1b585fb208847c9e6f1dd3c9d436
Alec <alec@24bitgames.com>
2018-08-30 11:16:57 +0200

    Code quality: input part 2

--- Assets/_Standard Assets/Characters/Scripts/CharacterInput/LegacyOnScreenCharacterInput.cs
+++ Assets/_Standard Assets/Characters/Scripts/CharacterInput/LegacyOnScreenCharacterInput.cs
@@ -1,27 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace StandardAssets.Characters.CharacterInput
 {
+	/// <summary>
+	/// Mobile onscreen "gamepad" implementation
+	/// </summary>
 	public class LegacyOnScreenCharacterInput : LegacyCharacterInputBase
 	{
-		[SerializeField]
-		protected StaticOnScreenJoystick leftOnScreenJoystick;
-		[SerializeField]
-		protected StaticOnScreenJoystick rightOnScreenJoystick;
+		[SerializeField, Tooltip("Reference to the onscreen joystick for movement")]
+		protected StaticOnScreenJoystick moveInputJoystick;
+		[SerializeField, Tooltip("Reference to the onscreen joystick for looking")]
+		protected StaticOnScreenJoystick lookInputJoystick;
 		
+		/// <inheritdoc />
 		protected override void UpdateLookVector()
 		{
-			Vector2 rightStickVector = rightOnScreenJoystick.GetStickVector();
+			Vector2 rightStickVector = lookInputJoystick.GetStickVector();
 			lookInputVector.x = -rightStickVector.x;
 			lookInputVector.y = -rightStickVector.y;
 		}
 
+		/// <inheritdoc />
 		protected override void UpdateMoveVector()
 		{
-			Vector2 leftStickVector = leftOnScreenJoystick.GetStickVector();		
+			Vector2 leftStickVector = moveInputJoystick.GetStickVector();		
 			moveInputVector.Set(leftStickVector.x, leftStickVector.y);
 		}
 		
+		/// <summary>
+		/// Used by jump Unity UI button 
+		/// </summary>
 		public void OnScreenTouchJump()
 		{
 			if (jumpPressed != null)
