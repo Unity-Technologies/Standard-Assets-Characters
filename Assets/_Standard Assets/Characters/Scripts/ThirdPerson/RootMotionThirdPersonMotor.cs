e2896280f136b748468c3c45b1d025ee39a7e531
Kevin Marais <kevin.n.marais@gmail.com>
2018-08-31 10:04:03 +0200

    Sprint can now be manually disabled

--- Assets/_Standard Assets/Characters/Scripts/ThirdPerson/RootMotionThirdPersonMotor.cs
+++ Assets/_Standard Assets/Characters/Scripts/ThirdPerson/RootMotionThirdPersonMotor.cs
@@ -254,7 +254,7 @@ namespace StandardAssets.Characters.ThirdPerson
 
 		private void OnSprintStarted()
 		{
-			sprint = true;
+			sprint = !sprint;
 		}
 		
 		private void OnSprintEnded()
@@ -301,7 +301,7 @@ namespace StandardAssets.Characters.ThirdPerson
 
 		public void Update()
 		{
-			if (sprint && !characterInput.hasMovementInput)
+			if (configuration.autoToggleSprintOnNoInput && sprint && !characterInput.hasMovementInput)
 			{
 				sprint = false;
 			}
