41330f5c3a19f2852328148b132cc30f64b6e6e1
Heinrich Mauer <heinrich@24bitgames.com>
2018-09-03 13:33:47 +0200

    Addressed review SC-CR-14. Added Documentation, comments and cleaned up code.

--- Assets/_Standard Assets/Characters/Scripts/GizmosHelpers/ArrowColorSelect.cs
+++ Assets/_Standard Assets/Characters/Scripts/GizmosHelpers/ArrowColorSelect.cs
@@ -1,31 +1,55 @@
-﻿using System.Security.AccessControl;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace StandardAssets.Characters.GizmosHelpers
-{ 
+{
+	/// <summary>
+	/// Attached to the GizmoArrow object to allow its colour to change in code
+	/// </summary>
 	public class ArrowColorSelect : MonoBehaviour
 	{
-		private MaterialPropertyBlock block;
+		/// <summary>
+		/// A collection of Mesh Renderers for all components of the GizmoArrow
+		/// </summary>
 		public MeshRenderer[] renderers;
 		
+		/// <summary>
+		/// Color selected for this instance of the GizmoArrow, defaulted to Red.
+		/// </summary>
 		public Color Color = Color.red;
 
+		/// <summary>
+		/// The collection of properties for all components of this instance of the GizmoArrow
+		/// </summary>
+		private MaterialPropertyBlock block;
+
+		/// <summary>
+		/// This function is called when the object becomes enabled and active.
+		/// </summary>
 		private void OnEnable()
 		{
 			UpdateColor();
 		}
 
+#if UNITY_EDITOR
+		/// <summary>
+		/// Runs the OnValidate Monobehaviour method
+		/// Public access modifier to allow OnValidate from <see cref="GizmoArrowHelper"/>
+		/// </summary>
 		public void OnValidate()
 		{
 			UpdateColor();
 		}
-		
-		
+#endif
 
-		void UpdateColor()
+		/// <summary>
+		/// Method to update the arrow color based on the color field.
+		/// </summary>
+		private void UpdateColor()
 		{
-			if(block == null)
-				block = new MaterialPropertyBlock(); 
+			if (block == null)
+			{
+				block = new MaterialPropertyBlock();
+			}
 			block.SetColor("_Color", Color);
 			foreach (var r in renderers)
 			{
