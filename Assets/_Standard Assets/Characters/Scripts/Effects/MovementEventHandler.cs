7e7fc09947cc54f53680184fa975ba4f4bcc3224
Alec <alec@24bitgames.com>
2018-10-08 10:07:17 +0200

    Inlined some data scripts

--- Assets/_Standard Assets/Characters/Scripts/Effects/MovementEventHandler.cs
+++ Assets/_Standard Assets/Characters/Scripts/Effects/MovementEventHandler.cs
@@ -116,4 +116,174 @@ namespace StandardAssets.Characters.Effects
 			}
 		}
 	}
+	
+	/// <summary>
+	/// Container of data associated with a movement event
+	/// </summary>
+	public struct MovementEventData
+	{
+		/// <summary>
+		/// Where the event was fired from
+		/// </summary>
+		public Transform firedFrom;
+
+		/// <summary>
+		/// The velocity that the effect occurs at
+		/// </summary>
+		public float normalizedSpeed;
+
+		/// <summary>
+		/// Constructs an instance of struct
+		/// </summary>
+		/// <param name="firedFromTransform">the transform of the emission of the movement - optional, default is null</param>
+		/// <param name="normalizedSpeedToUse">the normalized speed of the movement - optional, default is 0</param>
+		public MovementEventData(Transform firedFromTransform = null, float normalizedSpeedToUse = 0f)
+		{
+			firedFrom = firedFromTransform;
+			normalizedSpeed = normalizedSpeedToUse;
+		}
+	}
+	
+	/// <summary>
+	/// A library of movement effects.
+	/// This is what would be swapped out for different zones.
+	/// e.g. walking on dirt versus walking on metal.
+	/// </summary>
+	[Serializable]
+	public class MovementEventLibrary
+	{
+		[SerializeField, Tooltip("The movement event player prefab for handling left foot step")]
+		protected MovementEventPlayer leftFootStepPrefab;
+		
+		[SerializeField, Tooltip("The movement event player prefab for handling right foot step")]
+		protected MovementEventPlayer rightFootStepPrefab;
+
+		[SerializeField, Tooltip("The movement event player prefab for handling landing")]
+		protected MovementEventPlayer landingPrefab;
+
+		[SerializeField, Tooltip("The movement event player prefab for handling jumping")]
+		protected MovementEventPlayer jumpingPrefab;
+
+		/// <summary>
+		/// Cache of the various instances so that the prefab is only spawned once
+		/// </summary>
+		protected MovementEventPlayer leftFootStepInstance, rightFootStepInstance, landingInstance, jumpingInstance;
+
+		/// <summary>
+		/// Helper function for ensuring that the <see cref="MovementEventPlayer"/> prefab is only instantiated once and the cached version is then used
+		/// </summary>
+		/// <param name="movementEventData">The data relating to the movement event</param>
+		/// <param name="prefab">The prefab to instantiate, if it is not cached</param>
+		/// <param name="instance">The cached instance of the prefab - this could be null and therefore the keyword ref is required</param>
+		protected void PlayInstancedEvent(MovementEventData movementEventData, MovementEventPlayer prefab, ref MovementEventPlayer instance)
+		{
+			if (prefab == null)
+			{
+				return;
+			}
+
+			if (instance == null)
+			{
+				instance = GameObject.Instantiate(prefab);
+			}
+					
+			instance.Play(movementEventData);
+		}
+
+		/// <summary>
+		/// Helper for playing the Left Foot movement event
+		/// </summary>
+		/// <param name="movementEventData">The data relating to the movement event</param>
+		public void PlayLeftFoot(MovementEventData movementEventData)
+		{
+			PlayInstancedEvent(movementEventData, leftFootStepPrefab, ref leftFootStepInstance);
+		}
+		
+		/// <summary>
+		/// Helper for playing the Right Foot movement event
+		/// </summary>
+		/// <param name="movementEventData">The data relating to the movement event</param>
+		public void PlayRightFoot(MovementEventData movementEventData)
+		{
+			PlayInstancedEvent(movementEventData, rightFootStepPrefab, ref rightFootStepInstance);
+		}
+		
+		/// <summary>
+		/// Helper for playing the Landing movement event
+		/// </summary>
+		/// <param name="movementEventData">The data relating to the movement event</param>
+		public void PlayLanding(MovementEventData movementEventData)
+		{
+			PlayInstancedEvent(movementEventData, landingPrefab, ref landingInstance);
+		}
+		
+		/// <summary>
+		/// Helper for playing the Jumping movement event
+		/// </summary>
+		/// <param name="movementEventData">The data relating to the movement event</param>
+		public void PlayJumping(MovementEventData movementEventData)
+		{
+			PlayInstancedEvent(movementEventData, jumpingPrefab, ref jumpingInstance);
+		}
+	}
+	
+	/// <summary>
+	/// Defines which zone ID matches to which <see cref="MovementEventLibrary"/>
+	/// </summary>
+	[Serializable]
+	public class MovementEventZoneDefinition
+	{
+		[MovementZoneId, SerializeField, Tooltip("The ID of the zone used to play the effect")]
+		protected string zoneId = "concrete";
+
+		[SerializeField, Tooltip("The corresponding library of effects")]
+		protected MovementEventLibrary zoneLibrary;
+		
+		/// <summary>
+		/// Gets the zoneId
+		/// </summary>
+		public string id
+		{
+			get { return zoneId; }
+		}
+
+		/// <summary>
+		/// Gets the <see cref="MovementEventLibrary"/>
+		/// </summary>
+		public MovementEventLibrary library
+		{
+			get { return zoneLibrary; }
+		}
+	}
+	
+	/// <summary>
+	/// A set of <see cref="MovementEventLibrary"/> for different zone IDs
+	/// </summary>
+	[Serializable]
+	public class MovementEventZoneDefinitionList
+	{
+		[SerializeField, Tooltip("List of movement event libraries for different movement zones")]
+		protected MovementEventZoneDefinition[] movementZoneLibraries;
+		
+		/// <summary>
+		/// Gets the Gets the <see cref="MovementEventLibrary"/> for a specified zoneId for a specified zoneId
+		/// </summary>
+		/// <param name="zoneId">The zoneId needed to look up the <see cref="MovementEventLibrary"/></param>
+		/// <value>Gets the <see cref="MovementEventLibrary"/> for a specified zoneId. returns null if the zoneId does not have an associated <see cref="MovementEventLibrary"/></value>
+		public MovementEventLibrary this[string zoneId]
+		{
+			get
+			{
+				foreach (MovementEventZoneDefinition movementEventZoneDefinition in movementZoneLibraries)
+				{
+					if (movementEventZoneDefinition.id == zoneId)
+					{
+						return movementEventZoneDefinition.library;
+					}
+				}
+
+				return null;
+			}
+		}
+	}
 }
