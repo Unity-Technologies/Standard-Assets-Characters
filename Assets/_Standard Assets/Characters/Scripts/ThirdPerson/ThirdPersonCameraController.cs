5543b7034505b10016b5909ee1d0813b415a761f
Greg Labute <gregoryl@unity3d.com>
2018-11-12 16:21:52 -0500

    Restored the recenter functionality

--- Assets/_Standard Assets/Characters/Scripts/ThirdPerson/ThirdPersonCameraController.cs
+++ Assets/_Standard Assets/Characters/Scripts/ThirdPerson/ThirdPersonCameraController.cs
@@ -1,20 +1,56 @@
 ﻿using UnityEngine;
 using UnityEngine.Serialization;
+using Cinemachine;
 
 namespace StandardAssets.Characters.ThirdPerson
 {
 	/// <summary>
-	/// Displays crosshair when strafing
+	/// Displays crosshair when strafing, implement recentering on demand
 	/// </summary>
 	public class ThirdPersonCameraController : MonoBehaviour
 	{
-		[FormerlySerializedAs("crosshair")]
-		[SerializeField, Tooltip("The aiming crosshair that is visible during strafe")]
-		GameObject m_Crosshair;
+		[Tooltip("The aiming crosshair that is visible during strafe")]
+		public GameObject m_Crosshair;
 
 		public ThirdPersonBrain m_ThirdPersonBrain;
 
-		void Update()
+        CinemachineStateDrivenCamera m_SDC;
+
+        ThirdPersonInput UserInput
+        {
+            get
+            {
+                return (m_ThirdPersonBrain == null)
+                    ? null : m_ThirdPersonBrain.thirdPersonInput as ThirdPersonInput;
+            }
+        }
+
+        CinemachineFreeLook LiveFreeLook
+        {
+            get { return m_SDC == null ? null : m_SDC.LiveChild as CinemachineFreeLook; }
+        }
+
+        private void OnEnable()
+        {
+            var userInput = UserInput;
+			if (userInput != null)
+			{
+				userInput.recentreCamera -= RecenterCamera;
+				userInput.recentreCamera += RecenterCamera;
+			}
+            m_SDC = GetComponent<CinemachineStateDrivenCamera>();
+        }
+
+        private void OnDisable()
+        {
+            var userInput = UserInput;
+			if (userInput != null)
+			{
+				userInput.recentreCamera -= RecenterCamera;
+			}
+        }
+
+        void Update()
 		{
 			if (m_ThirdPersonBrain == null)
 			{
@@ -23,6 +59,10 @@ namespace StandardAssets.Characters.ThirdPerson
 				return;
 			}
             SetCrosshairVisible(m_ThirdPersonBrain.IsStrafing);
+
+            var userInput = UserInput;
+            if (userInput != null && (userInput.hasMovementInput || userInput.lookInput != Vector2.zero))
+                DisableRecentering(LiveFreeLook);
 		}
 
 		void SetCrosshairVisible(bool isVisible = true)
@@ -30,5 +70,30 @@ namespace StandardAssets.Characters.ThirdPerson
 			if (m_Crosshair != null)
     			m_Crosshair.SetActive(isVisible);
 		}
-	}
+
+        void RecenterCamera()
+        {
+            var userInput = UserInput;
+            if (userInput != null && !userInput.hasMovementInput)
+                EnableRecentering(LiveFreeLook);
+        }
+
+		void EnableRecentering(CinemachineFreeLook freeLook)
+		{
+            if (freeLook != null)
+            {
+			    freeLook.m_RecenterToTargetHeading.m_enabled = true;
+			    freeLook.m_YAxisRecentering.m_enabled = true;
+		    }
+        }
+
+		void DisableRecentering(CinemachineFreeLook freeLook)
+		{
+            if (freeLook != null)
+            {
+			    freeLook.m_RecenterToTargetHeading.m_enabled = false;
+			    freeLook.m_YAxisRecentering.m_enabled = false;
+            }
+		}
+    }
 }
