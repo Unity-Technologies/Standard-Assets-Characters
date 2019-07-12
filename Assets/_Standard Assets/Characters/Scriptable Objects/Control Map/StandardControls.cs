405cea43875ab751eb93d514392636e06ff1bfae
Dave Brookstein <david@24bitgames.com>
2019-07-12 15:33:49 +0200

    Separated gamepad analogue look and mouse look actions. Added a processor to scale mouse look vectors.

--- Assets/_Standard Assets/Characters/Scriptable Objects/Control Map/StandardControls.cs
+++ Assets/_Standard Assets/Characters/Scriptable Objects/Control Map/StandardControls.cs
@@ -1,6 +1,9 @@
 // GENERATED AUTOMATICALLY FROM 'Assets/_Standard Assets/Characters/Scriptable Objects/Control Map/StandardControls.inputactions'
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
@@ -17,18 +20,7 @@ public class StandardControls : IInputActionCollection
             ""id"": ""401f0562-653c-45d7-b6af-d43a16e5e984"",
             ""actions"": [
                 {
-                    ""name"": ""crouch"",
-                    ""id"": ""5800e495-214e-474b-b3e0-57242409f8c1"",
-                    ""expectedControlLayout"": """",
-                    ""continuous"": false,
-                    ""passThrough"": false,
-                    ""initialStateCheck"": false,
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""bindings"": []
-                },
-                {
-                    ""name"": ""look"",
+                    ""name"": ""gamepadLook"",
                     ""id"": ""18ffe753-4cc1-45fc-8377-f9b5292b1a4b"",
                     ""expectedControlLayout"": """",
                     ""continuous"": false,
@@ -41,6 +33,17 @@ public class StandardControls : IInputActionCollection
                 {
                     ""name"": ""move"",
                     ""id"": ""21a14e86-4fb7-49f9-af14-ddbfc9451078"",
+                    ""expectedControlLayout"": """",
+                    ""continuous"": false,
+                    ""passThrough"": false,
+                    ""initialStateCheck"": false,
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""bindings"": []
+                },
+                {
+                    ""name"": ""crouch"",
+                    ""id"": ""5800e495-214e-474b-b3e0-57242409f8c1"",
                     ""expectedControlLayout"": """",
                     ""continuous"": false,
                     ""passThrough"": false,
@@ -103,6 +106,17 @@ public class StandardControls : IInputActionCollection
                     ""processors"": """",
                     ""interactions"": """",
                     ""bindings"": []
+                },
+                {
+                    ""name"": ""mouseLook"",
+                    ""id"": ""94fe90c2-2b91-475e-9d50-bb9f6e833c98"",
+                    ""expectedControlLayout"": """",
+                    ""continuous"": false,
+                    ""passThrough"": false,
+                    ""initialStateCheck"": false,
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""bindings"": []
                 }
             ],
             ""bindings"": [
@@ -128,102 +142,6 @@ public class StandardControls : IInputActionCollection
                     ""action"": ""crouch"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""c8406335-017f-4795-bec7-089df9ddbf19"",
-                    ""path"": ""<Mouse>/delta"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""6350393e-2ea8-4399-88ad-38aa713e5cc5"",
-                    ""path"": ""<Gamepad>/rightStick"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""08a6f928-9ff7-4bfd-94a8-42d5c1e5965c"",
-                    ""path"": ""<Gamepad>/leftStick"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": ""Dpad"",
-                    ""id"": ""a0d87c4f-1156-4d0f-8f03-bcad93861151"",
-                    ""path"": ""2DVector"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""move"",
-                    ""isComposite"": true,
-                    ""isPartOfComposite"": false,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": ""up"",
-                    ""id"": ""21f6ae24-62f4-40dc-8d9d-3fdb6534456c"",
-                    ""path"": ""<Keyboard>/w"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""d4b88c44-0df0-4a3e-b363-14930e8dd521"",
-                    ""path"": ""<Keyboard>/s"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": ""left"",
-                    ""id"": ""e7855ea9-d1b8-4a10-8cea-c105801a6079"",
-                    ""path"": ""<Keyboard>/a"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true,
-                    ""modifiers"": """"
-                },
-                {
-                    ""name"": ""right"",
-                    ""id"": ""a00dce50-ead7-41fe-a956-351297d70946"",
-                    ""path"": ""<Keyboard>/d"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true,
                     ""modifiers"": """"
                 },
                 {
@@ -333,6 +251,102 @@ public class StandardControls : IInputActionCollection
                     ""isComposite"": false,
                     ""isPartOfComposite"": false,
                     ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""6350393e-2ea8-4399-88ad-38aa713e5cc5"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""gamepadLook"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""08a6f928-9ff7-4bfd-94a8-42d5c1e5965c"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""b0b67dc2-e40e-400e-b741-0ac7a433e00a"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""940c555b-5bf3-4002-a31e-37d5c07b02f6"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""6e468804-50f7-4d55-934f-b3a48b83bfed"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""9511bfd7-bfde-4706-9974-7e8e7322c223"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""1c9ac3f5-20aa-4d8d-8b0b-e5f0276aa445"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d0ea204a-b4ba-4d75-b646-ab2c4ed37ece"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": ""ScaleVector2(x=0.01,y=0.01)"",
+                    ""groups"": """",
+                    ""action"": ""mouseLook"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
                 }
             ]
         }
@@ -341,14 +355,15 @@ public class StandardControls : IInputActionCollection
 }");
         // Movement
         m_Movement = asset.GetActionMap("Movement");
-        m_Movement_crouch = m_Movement.GetAction("crouch");
-        m_Movement_look = m_Movement.GetAction("look");
+        m_Movement_gamepadLook = m_Movement.GetAction("gamepadLook");
         m_Movement_move = m_Movement.GetAction("move");
+        m_Movement_crouch = m_Movement.GetAction("crouch");
         m_Movement_recentre = m_Movement.GetAction("recentre");
         m_Movement_sprint = m_Movement.GetAction("sprint");
         m_Movement_strafe = m_Movement.GetAction("strafe");
         m_Movement_strafeToggle = m_Movement.GetAction("strafeToggle");
         m_Movement_jump = m_Movement.GetAction("jump");
+        m_Movement_mouseLook = m_Movement.GetAction("mouseLook");
     }
 
     ~StandardControls()
@@ -401,26 +416,28 @@ public class StandardControls : IInputActionCollection
     // Movement
     private InputActionMap m_Movement;
     private IMovementActions m_MovementActionsCallbackInterface;
-    private InputAction m_Movement_crouch;
-    private InputAction m_Movement_look;
+    private InputAction m_Movement_gamepadLook;
     private InputAction m_Movement_move;
+    private InputAction m_Movement_crouch;
     private InputAction m_Movement_recentre;
     private InputAction m_Movement_sprint;
     private InputAction m_Movement_strafe;
     private InputAction m_Movement_strafeToggle;
     private InputAction m_Movement_jump;
+    private InputAction m_Movement_mouseLook;
     public struct MovementActions
     {
         private StandardControls m_Wrapper;
         public MovementActions(StandardControls wrapper) { m_Wrapper = wrapper; }
-        public InputAction @crouch { get { return m_Wrapper.m_Movement_crouch; } }
-        public InputAction @look { get { return m_Wrapper.m_Movement_look; } }
+        public InputAction @gamepadLook { get { return m_Wrapper.m_Movement_gamepadLook; } }
         public InputAction @move { get { return m_Wrapper.m_Movement_move; } }
+        public InputAction @crouch { get { return m_Wrapper.m_Movement_crouch; } }
         public InputAction @recentre { get { return m_Wrapper.m_Movement_recentre; } }
         public InputAction @sprint { get { return m_Wrapper.m_Movement_sprint; } }
         public InputAction @strafe { get { return m_Wrapper.m_Movement_strafe; } }
         public InputAction @strafeToggle { get { return m_Wrapper.m_Movement_strafeToggle; } }
         public InputAction @jump { get { return m_Wrapper.m_Movement_jump; } }
+        public InputAction @mouseLook { get { return m_Wrapper.m_Movement_mouseLook; } }
         public InputActionMap Get() { return m_Wrapper.m_Movement; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -431,15 +448,15 @@ public class StandardControls : IInputActionCollection
         {
             if (m_Wrapper.m_MovementActionsCallbackInterface != null)
             {
-                crouch.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnCrouch;
-                crouch.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnCrouch;
-                crouch.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnCrouch;
-                look.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnLook;
-                look.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnLook;
-                look.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnLook;
+                gamepadLook.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnGamepadLook;
+                gamepadLook.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnGamepadLook;
+                gamepadLook.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnGamepadLook;
                 move.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
                 move.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
                 move.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+                crouch.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnCrouch;
+                crouch.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnCrouch;
+                crouch.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnCrouch;
                 recentre.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnRecentre;
                 recentre.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnRecentre;
                 recentre.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnRecentre;
@@ -455,19 +472,22 @@ public class StandardControls : IInputActionCollection
                 jump.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
                 jump.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
                 jump.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnJump;
+                mouseLook.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnMouseLook;
+                mouseLook.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnMouseLook;
+                mouseLook.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnMouseLook;
             }
             m_Wrapper.m_MovementActionsCallbackInterface = instance;
             if (instance != null)
             {
-                crouch.started += instance.OnCrouch;
-                crouch.performed += instance.OnCrouch;
-                crouch.canceled += instance.OnCrouch;
-                look.started += instance.OnLook;
-                look.performed += instance.OnLook;
-                look.canceled += instance.OnLook;
+                gamepadLook.started += instance.OnGamepadLook;
+                gamepadLook.performed += instance.OnGamepadLook;
+                gamepadLook.canceled += instance.OnGamepadLook;
                 move.started += instance.OnMove;
                 move.performed += instance.OnMove;
                 move.canceled += instance.OnMove;
+                crouch.started += instance.OnCrouch;
+                crouch.performed += instance.OnCrouch;
+                crouch.canceled += instance.OnCrouch;
                 recentre.started += instance.OnRecentre;
                 recentre.performed += instance.OnRecentre;
                 recentre.canceled += instance.OnRecentre;
@@ -483,6 +503,9 @@ public class StandardControls : IInputActionCollection
                 jump.started += instance.OnJump;
                 jump.performed += instance.OnJump;
                 jump.canceled += instance.OnJump;
+                mouseLook.started += instance.OnMouseLook;
+                mouseLook.performed += instance.OnMouseLook;
+                mouseLook.canceled += instance.OnMouseLook;
             }
         }
     }
@@ -495,13 +518,14 @@ public class StandardControls : IInputActionCollection
     }
     public interface IMovementActions
     {
-        void OnCrouch(InputAction.CallbackContext context);
-        void OnLook(InputAction.CallbackContext context);
+        void OnGamepadLook(InputAction.CallbackContext context);
         void OnMove(InputAction.CallbackContext context);
+        void OnCrouch(InputAction.CallbackContext context);
         void OnRecentre(InputAction.CallbackContext context);
         void OnSprint(InputAction.CallbackContext context);
         void OnStrafe(InputAction.CallbackContext context);
         void OnStrafeToggle(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
+        void OnMouseLook(InputAction.CallbackContext context);
     }
 }
